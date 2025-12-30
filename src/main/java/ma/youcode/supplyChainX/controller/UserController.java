bdbc1b601ce12606b7a06bcb5910baf030cd4346
unknown <ayoub1benmansour@gmail.com>
2025-12-30 15:54:33 +0100

    removed aspect auth

--- src/main/java/ma/youcode/supplyChainX/controller/UserController.java
+++ src/main/java/ma/youcode/supplyChainX/controller/UserController.java
@@ -3,7 +3,6 @@ package ma.youcode.supplyChainX.controller;
 import lombok.RequiredArgsConstructor;
 import ma.youcode.supplyChainX.dto.UserRequest;
 import ma.youcode.supplyChainX.dto.UserResponse;
-import ma.youcode.supplyChainX.security.SecuredAction;
 import ma.youcode.supplyChainX.service.UserService;
 import ma.youcode.supplyChainX.shared.enums.Role;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +16,11 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    @SecuredAction(roles = {Role.ADMIN})
     public ResponseEntity<UserResponse> createUser(@RequestBody UserRequest request) {
         return ResponseEntity.ok(userService.createUser(request));
     }
 
     @PutMapping("/{id}/role")
-    @SecuredAction(roles = {Role.ADMIN})
     public ResponseEntity<UserResponse> updateUserRole(
             @PathVariable Long id,
             @RequestParam Role newRole
